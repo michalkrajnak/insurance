aefbf536a09f6a2df1d5f83d0a3ade4a97f899cb
STIEVJ01 <jan.stievko@dixonscarphone.com>
2019-05-13 15:23:07 +0100

    [AVS-51] webpack configuration prepared

--- products/frontend/config/webpack.dev.js
+++ products/frontend/config/webpack.dev.js
@@ -9,3 +9,14 @@ module.exports = {
         contentBase: path.join(__dirname, '../dist')
     }
 }
+=======
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        open: true
+    }
+});
