48fc6d42ab8885c476ad62b2b2f0b434a69046ad
KOSTRJ03 <jan.kostrhun03@dixonscarphone.com>
2019-05-14 14:55:55 +0200

    [AVS-32] - implementation of first microfrontend

--- products/frontend/src/components/BasketConfigurationForm.jsx
+++ products/frontend/src/components/BasketConfigurationForm.jsx
@@ -10,14 +10,8 @@ class BasketConfigurationForm extends Component {
         this.state = {
             SB: false,
             BB: false,
-            SBRE: false
+            SBRE: false,
         }
-
-        this.onSubmit = this.onSubmit.bind(this)
-    }
-
-    onSubmit() {
-        this.props.onSelect(this.state)
     }
 
     render() {
