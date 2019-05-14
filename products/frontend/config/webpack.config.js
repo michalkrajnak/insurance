0612cac80061eb50e5bd261461849d30ff96956f
KOSTRJ03 <jan.kostrhun03@dixonscarphone.com>
2019-05-14 14:55:55 +0200

    [AVS-32] - implementation of first microfrontend

--- products/frontend/config/webpack.config.js
+++ products/frontend/config/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const Dotenv = require('dotenv-webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.jsx',
@@ -20,8 +20,7 @@ module.exports = {
             template: "./public/index.html"
         }),
         new CleanWebpackPlugin(),
-        new Dotenv(),
-        new CleanWebpackPlugin()
+        new Dotenv()
     ],
     module: {
         rules: [
